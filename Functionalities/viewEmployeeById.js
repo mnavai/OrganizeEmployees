c99f89786dc1c2a795fdc94b8067214fb7286ae3
Maryam Navaei <maryam.navaei@gmail.com>
2023-04-19 14:49:20 -0400

    find employee fixed

--- Functionalities/viewEmployeeById.js
+++ Functionalities/viewEmployeeById.js
@@ -1,19 +1,19 @@
 function viewEmployeeById(employees, rl, mainMenu) {
-    rl.question("Enter the ID of the employee you want to view: ", (id) => {
-      const employee = employees.find((emp) => emp.id === id);
-      if (employee) {
-        console.log(`Name: ${employee.name}`);
-        console.log(`Age: ${employee.age}`);
-        console.log(`ID: ${employee.id}`);
-        console.log(`Contact: ${employee.contact}`);
-        console.log(`Email: ${employee.email}`);
-        mainMenu();
-      } else {
-        console.log(`Employee with ID ${id} not found.`);
-        mainMenu();
-      }
-    });
-  }
-  
-  export default viewEmployeeById;
-  
+  rl.question("Enter the ID of the employee you want to view: ", (id) => {
+    const employee = employees.find((emp) => emp.id === parseInt(id));
+    if (employee) {
+      console.log(`Name: ${employee.name}`);
+      console.log(`Age: ${employee.age}`);
+      console.log(`ID: ${employee.id}`);
+      console.log(`Contact: ${employee.contact}`);
+      console.log(`Email: ${employee.email}`);
+      mainMenu();
+    } else {
+      console.log(`Employee with ID ${id} not found.`);
+      mainMenu();
+    }
+  });
+}
+
+export default viewEmployeeById;
+
